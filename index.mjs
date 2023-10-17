3a0cdbee2ae412dd936c9947d32827921f566a96
Arturo Abreu <r2abreu@protonmail.com>
2023-10-17 14:41:28 +0000

    chore: add get and post actions

--- index.mjs
+++ index.mjs
@@ -12,88 +12,17 @@ import { constants } from "node:fs";
 import { createReadStream } from "node:fs";
 import { createInterface } from "node:readline";
 import { Readable } from "node:stream";
+import messages from "./messages/index.mjs";
 
 const server = http.createServer();
 
 server.on("request", function(request, response) {
-    // Request: http.IncomingMessage
-    switch (request.url) {
-      case '/' :
-        handleGetRequest(request, response);
-        break;
-      case '/messages' : 
-        handlePostRequest(request, response);
-        break;
-    }
+  const url = new URL(request.url, `https://${request.headers.host}`)
+  switch (true) {
+    case /^\/messages(\/\d+)?$/.test(url.pathname) : 
+      messages[request.method](url, request, response);
+      break;
+  }
 });
 
-async function handleGetRequest(request, response) {
-  // show list of messages
-  try {
-    const file = await readFile('file', 'utf8'); 
-    response.writeHead(200);
-    response.end(file);
-  } catch (error) {
-    response.writeHead(500);
-    response.end("Unable to read file");
-  }
-};
-
-async function handlePostRequest(request, response) {
-  request.setEncoding('utf8');
-
-  let id = 0;
-  let body = "";
-
-  try {
-    const [ _body, _lastline ] = await readFileAndGetLastLine() || [];
-    
-    body = _body;
-    id = _lastline ? _lastline.split(";").join("")[0] : 0;
-  } catch (error) {
-    console.error(error);
-  }
-
-  let requestBody = "";
-  request.on("data", function(chunk) {
-    requestBody += `${++id};${chunk}\n`
-  })
-
-  request.on("end", async function() {
-    try {
-      await appendFile("file", requestBody);     
-      response.writeHead(200);
-      response.end(requestBody);
-    } catch (error)  {
-      console.error(error);
-      response.writeHead(500);
-      response.end('Error appending data to the file');
-    }
-  })
-};
-
-async function readFileAndGetLastLine() {
-  try { await access('file', constants.F_OK); } catch (e) { return console.error(e) }
-  const input = createReadStream('file', { encoding: 'utf8' }); 
-  return new Promise(function (resolve, reject) {
-    const readline = createInterface({ input });
-
-    let lastline = "";
-    let body = "";
-
-    readline.on("line", function(line) {
-      lastline=line;
-      body+=`${line}\n`;
-    })
-
-    readline.on("close", function() {
-      resolve([body, lastline]);
-    })
-
-    readline.on("error", function(error) {
-      reject(error);
-    })
-  })
-}
-
-server.listen(3000);
+server.listen('3000');
