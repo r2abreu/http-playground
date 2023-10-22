d34dd3bdacd38bd89301075d84407a0da0438580
Arturo Abreu <r2abreu@protonmail.com>
2023-10-22 16:37:45 +0000

    chore: fulfil crud methods

--- messages/index.mjs
+++ messages/index.mjs
@@ -1,4 +1,4 @@
-import { appendFile, readFile, access } from "node:fs/promises";
+import { writeFile, appendFile, readFile, access } from "node:fs/promises";
 import { constants } from "node:fs";
 import { createReadStream } from "node:fs";
 import { createInterface } from "node:readline";
@@ -8,19 +8,29 @@ export default {
   GET: function(url, request, response) {
     const id = url.pathname.split("/")[2];
 
-    id ? getMessageById(id, request, response) : getAll(request, response);
+    id ? getMessage(id, response) : getMessages(request, response);
   }, 
   POST: function(url, request, response) {
     createMessage(url, request, response);
-  }
+  },
+  DELETE: function(url, request, response) {
+    const id = url.pathname.split("/")[2];
+
+    deleteMessage(id, response);
+  },
+  PATCH: function(url, request, response) {
+    const id = url.pathname.split("/")[2];
+
+    updateMessage(id, request, response);
+  } 
 }
 
-async function getAll (request, response) {
+async function getMessages(request, response) {
   const [ file ] = await readFileAndGetLastLine() || [];
 
   if (!file) {
     response.writeHead(500);
-    response.end('Sorry, we were unable to read file contents');
+    response.end('Sorry, we were unable to read file contents\n');
     return;
   } 
   
@@ -28,7 +38,7 @@ async function getAll (request, response) {
   response.end(file);
 }
 
-async function getMessageById(id) {
+async function getMessage(id, response) {
   const [ file ] = await readFileAndGetLastLine() || [];
 
   if (!file) {
@@ -39,6 +49,111 @@ async function getMessageById(id) {
 
   const lines = file.split("\n");
   const entry = lines.find(line => line.split(';')[0] === id)
+
+  if (!entry) {
+    response.writeHead(400);
+    response.end('Entry not found');
+  }
+
+  response.writeHead(200);
+  response.end(`${entry}\n`);
+}
+
+
+async function createMessage(url, request, response) {
+  request.setEncoding('utf8');
+
+  let id = 0;
+  let body = "";
+
+  try {
+    const [ _body, _lastline ] = await readFileAndGetLastLine() || [];
+    body = _body;
+    id = _lastline ? _lastline.split(";")[0] : 0;
+  } catch (error) {
+    console.error(error);
+  }
+
+  let requestBody = "";
+  request.on("data", function(chunk) {
+    requestBody += `${++id};${chunk}\n`
+  })
+
+  request.on("end", async function() {
+    try {
+      await appendFile("file", requestBody);     
+      response.writeHead(201, {
+        "Location": `${url.pathname}/${id}`
+      });
+      response.end(requestBody);
+    } catch (error)  {
+      console.error(error);
+      response.writeHead(500);
+      response.end('Error appending data to the file');
+    }
+  })
+};
+
+async function deleteMessage(id, response) {
+  const [ file ] = await readFileAndGetLastLine() || [];
+
+  if (!file) {
+    response.writeHead(500);
+    response.end('Sorry, we were unable to read file contents');
+    return;
+  } 
+
+  const lines = file.split("\n");
+  const filtered = lines.filter((line) => line.split(';')[0] !== id)
+  const newFile = filtered.join("\n");
+  // first we have to make sure that the entry exists (maybe not)
+  try {
+    await writeFile('file', newFile);
+    response.writeHead(200);
+    response.end();
+  } catch (error) {
+    console.log(error);
+    response.writeHead(500);
+    response.end("Sorry, unable to delete entry\n");
+  }
+}
+
+async function updateMessage(id, request, response) {
+  const [ file ] = await readFileAndGetLastLine() || [];
+
+  if (!file) {
+    response.writeHead(500);
+    response.end('Sorry, we were unable to read file contents');
+    return;
+  } 
+
+  const lines = file.split("\n");
+  const entry = lines.find((line) => line.split(';')[0] === id)
+  const entryId = entry.split(';')[0];
+  const indexOfEntry = lines.indexOf(entry);
+
+  if (!entry) {
+    response.writeHead(400);
+    response.end("Entry not found");
+  }
+
+  let requestBody = "";
+  request.on("data", function(chunk) {
+    requestBody += `${entryId};${chunk}`
+  })
+
+  request.on("end", async function(chunk) {
+    try {
+      const replaced = file.replace(entry, requestBody);
+      const newFile = await writeFile('file', replaced);
+      response.writeHead(200);
+      response.end(`${requestBody}\n`);
+    } catch (error) {
+      console.error(error); 
+      response.writeHead(500);
+      response.end("Sorry, error updating the entity");
+    }
+  }) 
 }
 
 async function readFileAndGetLastLine() {
@@ -64,39 +179,3 @@ async function readFileAndGetLastLine() {
     })
   })
 }
-
-
-async function createMessage(url, request, response) {
-  request.setEncoding('utf8');
-
-  let id = 0;
-  let body = "";
-
-  try {
-    const [ _body, _lastline ] = await readFileAndGetLastLine() || [];
-    body = _body;
-    id = _lastline ? _lastline.split(";").join("")[0] : 0;
-  } catch (error) {
-    console.error(error);
-  }
-
-  let requestBody = "";
-  request.on("data", function(chunk) {
-    requestBody += `${++id};${chunk}\n`
-  })
-
-  request.on("end", async function() {
-    try {
-      await appendFile("file", requestBody);     
-      response.writeHead(201, {
-        "Location": `${url.pathname}/${id}`
-      });
-      response.end(requestBody);
-    } catch (error)  {
-      console.error(error);
-      response.writeHead(500);
-      response.end('Error appending data to the file');
-    }
-  })
-};
-
