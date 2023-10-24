99c6412044719179071e62f6a14b1f45835b2b14
Arturo Abreu <r2abreu@protonmail.com>
2023-10-24 14:31:24 +0000

    chore: fix conflicts
    Testing should be added soon

--- messages/index.mjs
+++ messages/index.mjs
@@ -1,160 +1,95 @@
-<<<<<<< Updated upstream
-import { appendFile, readFile, access } from "node:fs/promises";
-import { constants } from "node:fs";
-import { createReadStream } from "node:fs";
-import { createInterface } from "node:readline";
-import { Readable } from "node:stream";
-=======
 import { query } from "../db/index.mjs";
->>>>>>> Stashed changes
 
 export default {
-  GET: function(url, request, response) {
+  GET: function (url, _, response) {
     const id = url.pathname.split("/")[2];
 
-    id ? getMessageById(id, request, response) : getAll(request, response);
-  }, 
-  POST: function(url, request, response) {
-    createMessage(url, request, response);
-  }
-}
+    id ? getMessage(id, response) : getMessages(response);
+  },
+  POST: function (_, request, response) {
+    createMessage(request, response);
+  },
+  DELETE: function (url, _, response) {
+    const id = url.pathname.split("/")[2];
 
-<<<<<<< Updated upstream
-async function getAll (request, response) {
-  const [ file ] = await readFileAndGetLastLine() || [];
+    deleteMessage(id, response);
+  },
+  PATCH: function (url, request, response) {
+    const id = url.pathname.split("/")[2];
 
-  if (!file) {
-    response.writeHead(500);
-    response.end('Sorry, we were unable to read file contents');
-=======
-async function getMessages(request, response) {
+    updateMessage(id, request, response);
+  },
+};
+
+async function getMessages(response) {
   try {
-    const getQuery = 'SELECT * FROM message';
+    const getQuery = "SELECT * FROM message";
     const results = await query(getQuery);
-    const responseMessage = !results.rowCount 
-      ? "No messages" 
-      : results.rows.map(({id, content}) => `${id}:${content}\n`).join("")
+    const responseMessage = !results.rowCount
+      ? "No messages"
+      : results.rows.map(({ id, content }) => `${id}:${content}\n`).join("");
     response.writeHead(200);
     response.end(`${responseMessage}\n`);
   } catch (error) {
     console.error(error);
     response.writeHead(500);
-    response.end('Sorry, we were unable to read db contents\n');
->>>>>>> Stashed changes
+    response.end("Sorry, we were unable to read db contents\n");
     return;
   }
-  
 }
 
-<<<<<<< Updated upstream
-async function getMessageById(id) {
-  const [ file ] = await readFileAndGetLastLine() || [];
-
-  if (!file) {
-    response.writeHead(500);
-    response.end('Sorry, we were unable to read file contents');
-    return;
-  } 
-
-  const lines = file.split("\n");
-  const entry = lines.find(line => line.split(';')[0] === id)
-}
-
-async function readFileAndGetLastLine() {
-  try { await access('file', constants.F_OK); } catch (e) { return console.error(e) }
-  const input = createReadStream('file', { encoding: 'utf8' }); 
-  return new Promise(function (resolve, reject) {
-    const readline = createInterface({ input });
-
-    let lastline = "";
-    let fileContents = "";
-
-    readline.on("line", function(line) {
-      lastline=line;
-      fileContents+=`${line}\n`;
-    })
-
-    readline.on("close", function() {
-      resolve([fileContents, lastline]);
-    })
-
-    readline.on("error", function(error) {
-      reject(error);
-    })
-  })
-}
-=======
 async function getMessage(id, response) {
-    try {
-      const getQuery = 'SELECT * FROM message WHERE id = $1';
-      const results = await query(getQuery, [id]);
-      if (!results.rowCount) {
-        response.writeHead(404);
-        response.end("Not found\n");
-      } else {
-        const { id, content } = results.rows[0];
-        response.writeHead(200);
-        response.end(`${id}:${content}\n`);
-      }
-    } catch (error) {
-      console.error(error);
-      response.writeHead(500)
-      response.end("Error retrieving message");
-    }
-} 
->>>>>>> Stashed changes
-
-
-async function createMessage(url, request, response) {
-  request.setEncoding('utf8');
-<<<<<<< Updated upstream
-
-  let id = 0;
-  let body = "";
-
   try {
-    const [ _body, _lastline ] = await readFileAndGetLastLine() || [];
-    body = _body;
-    id = _lastline ? _lastline.split(";").join("")[0] : 0;
+    const getQuery = "SELECT * FROM message WHERE id = $1";
+    const results = await query(getQuery, [id]);
+    if (!results.rowCount) {
+      response.writeHead(404);
+      response.end("Not found\n");
+    } else {
+      const { id, content } = results.rows[0];
+      response.writeHead(200);
+      response.end(`${id}:${content}\n`);
+    }
   } catch (error) {
     console.error(error);
+    response.writeHead(500);
+    response.end("Error retrieving message");
   }
+}
 
-  let requestBody = "";
-  request.on("data", function(chunk) {
-    requestBody += `${++id};${chunk}\n`
-  })
+async function createMessage(request, response) {
+  request.setEncoding("utf8");
+  let body = "";
+  request.on("data", (chunk) => (body += chunk));
+  request.on("end", async () => {
+    if (!body) {
+      response.writeHead(400);
+      response.end("Message content is required\n");
+      return;
+    }
 
-  request.on("end", async function() {
-=======
-  let body="";
-  request.on('data', (chunk) => body+=chunk);
-  request.on('end', async () => {
->>>>>>> Stashed changes
     try {
-      const queryPost = 'INSERT INTO message (content) VALUES ($1) RETURNING *';
+      const queryPost = "INSERT INTO message (content) VALUES ($1) RETURNING *";
       const results = await query(queryPost, [body]);
       const { id, content } = results.rows[0];
-      response.writeHead(200, { "Location": `http://${request.host}/${id}`})
+      response.writeHead(200, { Location: `http://${request.host}/${id}` });
       response.end(`${id}:${content}\n`);
     } catch (error) {
       response.writeHead(500);
       response.end("Error creating message");
     }
-  })
-};
+  });
+}
 
-<<<<<<< Updated upstream
-=======
 async function deleteMessage(id, response) {
   if (!id) {
     response.writeHead(400);
     response.end("Id parameter is required\n");
     return;
-  }
+ }
 
   try {
-    const queryDelete = 'DELETE FROM message WHERE id = $1'
+    const queryDelete = "DELETE FROM message WHERE id = $1";
     const result = await query(queryDelete, [id]);
     response.writeHead(200);
     response.end("OK\n");
@@ -172,23 +107,25 @@ async function updateMessage(id, request, response) {
     return;
   }
 
-  request.setEncoding('utf8');
+  request.setEncoding("utf8");
 
   let body = "";
-  request.on("data", (chunk) => body+=chunk);
+  request.on("data", (chunk) => (body += chunk));
 
-  request.on("end", async function(chunk) {
+  request.on("end", async function () {
     try {
-      const queryUpdate = 'UPDATE message SET content = $1 where ID = $2 RETURNING *';
+      const queryUpdate =
+        "UPDATE message SET content = $1 where ID = $2 RETURNING *";
       const results = await query(queryUpdate, [body, id]);
       const { id: resultId, content } = results.rows[0];
-      response.writeHead(200, { "Location": `http://${request.host}/messages/${resultId}`});
+      response.writeHead(200, {
+        Location: `http://${request.host}/messages/${resultId}`,
+      });
       response.end(`${resultId}:${content}\n`);
     } catch (error) {
-      console.error(error); 
+      console.error(error);
       response.writeHead(500);
       response.end("Error updating the entity\n");
     }
-  }) 
+  });
 }
->>>>>>> Stashed changes
